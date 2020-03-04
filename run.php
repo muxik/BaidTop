809731053f9915eff031471b09df986f4c54a399
muxi_k <lqjxm6662@163.com>
2020-03-04 19:55:38 +0800

    爬取结果

---
+++ run.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ .'/Request.php';
+
+// 运行次数
+$run_num = $argv[2];
+
+//运行时间间隔
+$interval = $argv[3];
+
+if ($argv[1] == 'start'){
+    $run = new Request($run_num,$interval);
+    $run->start();
+}else {
+    echo "Error:参数错误！\n请使用php run.php start [运行次数] [时间间隔] 开始运行\n";
+}
